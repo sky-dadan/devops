41fcc6f689de33e6eb2e8393dd00c046dec1d006
zhangxunan <zhangxunan@yuanxin-inc.com>
2015-11-05 14:09:52 +0800

    lock.py /api/lock_user修改为PUT方法,并添加用户验证

--- devops/api/lock.py
+++ devops/api/lock.py
@@ -14,18 +14,34 @@ def is_locked(username):
     row = app.config['cursor'].fetchone()
     return json.dumps({'code':0, 'is_lock':row[0]})
 
-@app.route("/api/lock_user",methods=['GET'])
+@app.route("/api/lock_user",methods=['GET','PUT'])
 def lock_user():
-    users = ['zhangxunan','lisi']
-#    for user in users:
-#        sql = 'update user set is_lock = 1 where username = "%s"' % user
-#        app.config['cursor'].execute(sql)
-#        util.write_log('zhangxunan','lock user %s' % user)
-#    return json.dumps("lock user")
+    try:
+        authorization = request.headers['authorization']
+        name = util.validate(authorization, app.config['passport_key'])
+        if not name:
+            logging.getLogger().warning("Request forbiden")
+            return json.dumps({'code': 1, 'errmsg': 'User validate error'})
+    except:
+        logging.getLogger().warning("Validate error: %s" % traceback.format_exc())
+        return json.dumps({'code': 1, 'errmsg': 'User validate error'})
 
-    sql='update user set is_lock = 1 where username in (%s)'
-    inargs = ', '.join(map(lambda x: '%s',users))
-    sql = sql % inargs
-    app.config['cursor'].execute_arg(sql,users)
-    util.write_log('zhangxunan','lock user %s' % ','.join(users))
-    return json.dumps('lock users')
+    try:
+        data = request.get_data()
+        data = json.loads(data)
+        users = []
+        for k, v in data.items():
+            users.append(v)
+
+        sql = 'update user set is_lock = 1 where username in (%s)'
+        inargs = ', '.join(map(lambda x: '%s',users))
+        sql = sql % inargs
+        app.config['cursor'].execute_arg(sql,users)
+
+        util.write_log(name,'lock user %s' % ','.join(users))
+        return json.dumps({'code':0,'result': 'Lock %s Success' % ','.join(users)})
+    except:
+        logging.getLogger().error("Lock user error: %s" % traceback.format_exc())
+        return json.dumps({'code': 1, 'errmsg': 'Lock user error'})
+
+    return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method})

9d79df825ed884946956662cea70b1ee7eca946a
zhangxunan <zhangxunan@yuanxin-inc.com>
2015-11-05 11:51:06 +0800

    添加批量锁定功能,update语句使用 where in ()

--- devops/api/lock.py
+++ devops/api/lock.py
@@ -7,11 +7,25 @@ import util
 
 from flask import Flask, request
 from . import app             #等价 from api import app
-dic={}
 @app.route("/api/islocked/<username>",methods=['GET', 'PUT'])
 def is_locked(username):
     sql='select is_lock from user where username = "%s"' % username
     app.config['cursor'].execute(sql)
     row = app.config['cursor'].fetchone()
-    dic['name']= row[0]
-    return json.dumps(dic)
+    return json.dumps({'code':0, 'is_lock':row[0]})
+
+@app.route("/api/lock_user",methods=['GET'])
+def lock_user():
+    users = ['zhangxunan','lisi']
+#    for user in users:
+#        sql = 'update user set is_lock = 1 where username = "%s"' % user
+#        app.config['cursor'].execute(sql)
+#        util.write_log('zhangxunan','lock user %s' % user)
+#    return json.dumps("lock user")
+
+    sql='update user set is_lock = 1 where username in (%s)'
+    inargs = ', '.join(map(lambda x: '%s',users))
+    sql = sql % inargs
+    app.config['cursor'].execute_arg(sql,users)
+    util.write_log('zhangxunan','lock user %s' % ','.join(users))
+    return json.dumps('lock users')

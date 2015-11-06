c0407831f6c1bbe244ae74279bec507ea4b5ab82
zhangxunan <zhangxunan@yuanxin-inc.com>
2015-11-06 16:38:14 +0800

    添加查询组列表的方法（路由）,并将groupadd.py改名为group.py

--- devops/api/group.py
+++ devops/api/group.py
@@ -51,3 +51,30 @@ def groupadd():
         return json.dumps({'code': 1, 'errmsg': 'Add group error'})
 
     return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method})
+
+@app.route('/api/querygroup',methods=['GET'])
+def query_group():
+    try:
+        authorization = request.headers['authorization']
+        name = util.validate(authorization, app.config['passport_key'])
+        if not name:
+            logging.getLogger().warning("Request forbiden")
+            return json.dumps({'code': 1, 'errmsg': 'User validate error'})
+    except:
+        logging.getLogger().warning("Validate error: %s" % traceback.format_exc())
+        return json.dumps({'code': 1, 'errmsg': 'User validate error'})
+    try:
+        fields=['group_name','user_name','group_comment']
+        groups = []
+        sql ='select groups.name as %s,user.name as %s,groups.comment as %s from groups,user,user_group where groups.id=user_group.group_id and user.id = user_group.user_id' %(fields[0],fields[1],fields[2])
+        app.config['cursor'].execute(sql)
+        for row in app.config['cursor'].fetchall():
+            group_list = {}
+            for i,k in enumerate(fields):
+                group_list[k] = row[i]
+            groups.append(group_list)
+        util.write_log(name, 'list all groups and memebers')
+        return json.dumps({'code': 0, 'groups': groups})
+    except:
+        logging.getLogger().error("Get group list error: %s" % traceback.format_exc())
+        return json.dumps({'code': 1, 'errmsg': 'Get group list error'})

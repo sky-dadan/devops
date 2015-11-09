a1c7a9a7a5786757c15f2a6b46766aee78b55542
zhangxunan <zhangxunan@yuanxin-inc.com>
2015-11-09 19:40:17 +0800

    change group.py

--- devops/api/group.py
+++ devops/api/group.py
@@ -8,73 +8,71 @@ import util
 from flask import Flask, request
 from . import app             #等价 from api import app
 
-@app.route("/api/groupadd",methods=['GET','PUT'])
-def groupadd():
+@app.route("/api/group",methods=['GET','POST'])
+def group():
     try:
         authorization = request.headers['authorization']
         name = util.validate(authorization, app.config['passport_key'])
         if not name:
             logging.getLogger().warning("Request forbiden")
             return json.dumps({'code': 1, 'errmsg': 'User validate error'})
-    except:
-        logging.getLogger().warning("Validate error: %s" % traceback.format_exc())
-        return json.dumps({'code': 1, 'errmsg': 'User validate error'})
-
-    try:
-        data = request.get_data()
-        data = json.loads(data)
-        users = []
-        groupname = data['groupname']
-        name_cn = data['name_cn']
-        comment = data['comment']
-        users = data['users']
-        if len(users) == 0:
-            return json.dumps({'code':1,'result':'Users can not be empty'})
-        sql = "insert into groups(name,name_cn,comment) values('%s','%s','%s')" % (groupname,name_cn,comment)
-        app.config['cursor'].execute(sql)
-        sql = 'select id from groups where name = "%s"' % groupname
+        sql = 'SELECT role FROM user WHERE username = "%s"' % (name)
         app.config['cursor'].execute(sql)
         row = app.config['cursor'].fetchone()
-        gid = row[0]
-        for uname in users:
-            sql = 'select id from user where username = "%s"' % uname
-            app.config['cursor'].execute(sql)
-            row = app.config['cursor'].fetchone()
-            uid = row[0]
-            sql = 'insert into user_group(user_id,group_id) values(%s,%s)' % (uid,gid)
-            app.config['cursor'].execute(sql)
-
-        util.write_log(name,'add group %s' % groupname)
-        return json.dumps({'code':0,'result': 'add group %s Success' % groupname})
-    except:
-        logging.getLogger().error("Lock user error: %s" % traceback.format_exc())
-        return json.dumps({'code': 1, 'errmsg': 'Add group error'})
-
-    return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method})
-
-@app.route('/api/querygroup',methods=['GET'])
-def query_group():
-    try:
-        authorization = request.headers['authorization']
-        name = util.validate(authorization, app.config['passport_key'])
-        if not name:
-            logging.getLogger().warning("Request forbiden")
-            return json.dumps({'code': 1, 'errmsg': 'User validate error'})
+        if row[0] == 1:
+            util.write_log(name,'%s is not admin,call failed' % (name))
+            return json.dumps({'code':1,'errmsg':'%s is not admin,call failed' % (name)})
+            sys.exit(1)
     except:
         logging.getLogger().warning("Validate error: %s" % traceback.format_exc())
         return json.dumps({'code': 1, 'errmsg': 'User validate error'})
-    try:
-        fields=['group_name','user_name','group_comment']
-        groups = []
-        sql ='select groups.name as %s,user.name as %s,groups.comment as %s from groups,user,user_group where groups.id=user_group.group_id and user.id = user_group.user_id' %(fields[0],fields[1],fields[2])
-        app.config['cursor'].execute(sql)
-        for row in app.config['cursor'].fetchall():
-            group_list = {}
-            for i,k in enumerate(fields):
-                group_list[k] = row[i]
-            groups.append(group_list)
-        util.write_log(name, 'list all groups and memebers')
-        return json.dumps({'code': 0, 'groups': groups})
-    except:
-        logging.getLogger().error("Get group list error: %s" % traceback.format_exc())
-        return json.dumps({'code': 1, 'errmsg': 'Get group list error'})
+
+    if request.method == 'GET':
+        try:
+            fields=['group_name','user_name','group_comment']
+            groups = []
+            sql ='SELECT groups.name AS %s,user.username AS %s,groups.comment AS %s FROM groups,user,user_group WHERE groups.id=user_group.group_id and user.id = user_group.user_id' %(fields[0],fields[1],fields[2])
+            app.config['cursor'].execute(sql)
+            for row in app.config['cursor'].fetchall():
+                group_list = {}
+                for i,k in enumerate(fields):
+                    group_list[k] = row[i]
+                groups.append(group_list)
+            util.write_log(name, 'list all groups and memebers')
+            return json.dumps({'code': 0, 'groups': groups})
+        except:
+            logging.getLogger().error("Get group list error: %s" % traceback.format_exc())
+            return json.dumps({'code': 1, 'errmsg': 'Get group list error'})
+
+    if request.method == 'POST':
+        try:
+            data = request.get_data()
+            data = json.loads(data)
+            fields = ['name','name_cn','comment']
+            uids = []
+            if len(data['users']) == 0:
+                return json.dumps({'code':1,'result':'Users can not be empty'})
+            sql = 'INSERT INTO groups (%s) VALUES (%s)' % (','.join(fields), ','.join(["'%s'" % data[x] for x in fields]))
+            app.config['cursor'].execute(sql)
+            sql = 'SELECT id FROM groups WHERE name = "%s"' % data['name']
+            app.config['cursor'].execute(sql)
+            row = app.config['cursor'].fetchone()
+            gid = row[0]
+            sql = "SELECT id FROM user WHERE username IN (%s)" % (','.join(["'%s'" % x for x in data['users']]))
+            print sql
+            app.config['cursor'].execute(sql)
+            rows = app.config['cursor'].fetchall()
+            for row in rows:
+                uids.append(row[0])
+            print uids
+            values = ', '.join(["(%s, %s)" % (gid, uid) for uid in uids])
+            print values
+            sql = "INSERT INTO user_group VALUES %s" % values
+            app.config['cursor'].execute(sql)
+            util.write_log(name,'add group %s' % data['name'])
+            return json.dumps({'code':0,'result': 'add group %s Success' % data['name']})
+        except:
+            logging.getLogger().error("Lock user error: %s" % traceback.format_exc())
+            return json.dumps({'code': 1, 'errmsg': 'Add group error'})
+
+    return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method})

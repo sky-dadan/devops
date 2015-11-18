aef5c2807e50475881dd0492cb04e3659c324b52
zp <you@example.com>
2015-11-18 19:30:42 +0800

    重构user.py，所有请求统一url.更新测试文件req.py

--- devops/api/user.py
+++ devops/api/user.py
@@ -1,11 +1,11 @@
 #coding:utf-8
 from flask import Flask,request
 from . import app
-import  json,traceback,hashlib
+import  json,traceback,hashlib,random
 import logging,util
 
-@app.route('/api/user/<int:user_id>',methods=['GET','PUT','DELETE'])
-def User(user_id):
+@app.route('/api/user',methods=['GET','PUT','POST','DELETE'])
+def User(offset=0,size=10):
     try:
         authorization = request.headers['authorization']
         name = util.validate(authorization,app.config['passport_key'])
@@ -16,44 +16,98 @@ def User(user_id):
         logging.getLogger().warning('validate error: %s' % traceback.format_exc())
         return json.dumps({'code':1,'errmsg':'User validate error'})
     
+###########  select for admin and user #################################
     if request.method == 'GET':  #get one user info from user_id
         try:
-            if not util.if_userid_exist(user_id):
-              return json.dumps({'code':1,'errmsg':'User is not exist'})
             user = {}
-            fields = ['id','username','name','email','mobile','role']
-            sql = "SELECT %s FROM user where id=%d" % (','.join(fields),user_id)
-            app.config['cursor'].execute(sql)
-            res = app.config['cursor'].fetchone()    #返回结果为元组(id,username,……)
-            for i,k in enumerate(fields):     #取出元组的值及对应的索引号 0.1,2……
-                user[k]=res[i]    #fields中的列名作为user字典的k,索引作为数据库返回列表的k,实现字典赋值
-            util.write_log(name, 'get_one_users') 
-            return json.dumps({'code':0,'users':user})
+            users = []
+            fields = ['username','name','email','mobile','role']
+            if util.role(name):  #admin select user list
+                if request.args.get('offset')  is not  None and request.args.get('size') is  not None:
+                    offset = request.args.get('offset')
+                    size = request.args.get('size')
+                    if int(size) >= 1000:
+                        return json.dumps({'code': 1, 'errmsg': 'you input size too big '})
+                sql = "SELECT %s FROM user limit %d,%d" % (','.join(fields),int(offset),int(size))
+                app.config['cursor'].execute(sql)
+                for row in app.config['cursor'].fetchall():
+                    for i, k in enumerate(fields):
+                        user[k] = row[i]
+                    users.append(user)
+                app.config['cursor'].execute("select  count(0) from user")
+                count=int(app.config['cursor'].fetchone()[0])
+                util.write_log(name, 'get_all_users')
+                return json.dumps({'code': 0, 'users': users,'count':count})
+            else:    #user select info 
+                sql = 'SELECT %s FROM user where username="%s"' % (','.join(fields),name)
+                app.config['cursor'].execute(sql)
+                res = app.config['cursor'].fetchone()    #返回结果为元组(id,username,……)
+                for i,k in enumerate(fields):     #取出元组的值及对应的索引号 0.1,2……
+                    user[k]=res[i]    #fields中的列名作为user字典的k,索引作为数据库返回列表的k,实现字典赋值
+                util.write_log(name, 'get_one_users') 
+                return json.dumps({'code':0,'users':user})
         except:
             logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
             return json.dumps({'code':1,'errmsg':'Get users error'})
-    elif request.method == 'PUT':   #user update 
+
+##############  update user for admin and user ################################
+    elif request.method == 'PUT':
         try:
             data = request.get_json()
             data = json.loads(data)
             if  util.role(name):    #admin  update from user_id
-                if not util.if_userid_exist(user_id): 
-                    return json.dumps({'code':1,'errmsg':'User is not exist'})
-                sql = 'update user set username="%(username)s",name="%(name)s",email="%(email)s",mobile="%(mobile)s" where id=%%d' % data %user_id
-            else:                      #login user update from his name
-                sql = 'update user set username="%(username)s",name="%(name)s",email="%(email)s",mobile="%(mobile)s" where username="%%s"' % data %name
+                if not  data.has_key('user_id'):
+                    return json.dumps({'code':1,'errmsg':'must input user_id'})
+                else:
+                    user_id = data['user_id']
+                    if not util.if_userid_exist(user_id): 
+                        return json.dumps({'code':1,'errmsg':'User is not exist'})
+                    sql = 'update user set username="%(username)s",name="%(name)s", \
+                            email="%(email)s",mobile="%(mobile)s" where id=%%d' % data %user_id
+            else:                      #user update from his name
+                sql = 'update user set username="%(username)s",name="%(name)s",email="%(email)s", \
+                        mobile="%(mobile)s" where username="%%s"' % data %name
             app.config['cursor'].execute(sql)
             util.write_log(name,'update user %s' % data['username'])
             return json.dumps({'code':0,'result':'update %s success' % data['username']})
         except:
             logging.getLogger().error('update user error : %s' % traceback.format_exc())
             return json.dumps({'code':1,'errmsg':'update user error'})
+
+#############  Create user for admin ################################
+    elif request.method == 'POST':
+        try:
+            if not util.role(name):
+                return json.dumps({'code':1,'errmsg':'you not admin '})
+            data = request.get_json()
+            data = json.loads(data)
+            fields, values = [], []
+            password = '3b53871ffb407966fc330307500ce968'
+            for k, v in data.items():
+                fields.append(k)
+                values.append("'%s'" % v)
+            sql = "INSERT INTO user (%s,password) VALUES (%s,'%s')" %  \
+                    (','.join(fields), ','.join(values),password)
+            app.config['cursor'].execute(sql)
+            util.write_log(name, "create_user %s" % data['username'])
+            return json.dumps({'code': 0, 'result': 'Create %s success' % data['username']})
+        except:
+            logging.getLogger().error("Create user error: %s" % traceback.format_exc())
+            return json.dumps({'code': 1, 'errmsg': 'Create user error'})
+
+#############  DELETE  user for admin ###########################
     elif request.method == 'DELETE':
         try:
+            data = request.get_json()
+            data = json.loads(data)
             if not util.role(name):
                 logging.getLogger().warning("You are not admin,Request forbiden")
                 return json.dumps({'code':1,'errmsg':'You are not admin,Request forbiden'})
-
+            else:
+                if not  data.has_key('user_id'):
+                    return json.dumps({'code':1,'errmsg':'must input user_id'})
+                else:
+                    user_id = data['user_id']
             if not util.if_userid_exist(user_id):
                 return json.dumps({'code':1,'errmsg':'User is not exist'})
             sql = "DELETE FROM user where id = %d" % (user_id) 
@@ -63,68 +117,5 @@ def User(user_id):
         except:
             logging.getLogger().error('delete user error : %s' % traceback.format_exc())
             return json.dumps({'code':1,'errmsg':'delete user error'})
-    return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method})
-
-
-@app.route('/api/user',methods=['GET','POST'])
-def UserList(offset=0,size=10):
-    try:
-        authorization = request.headers['authorization']
-        name = util.validate(authorization, app.config['passport_key'])
-        if not name:
-            logging.getLogger().warning("Request forbiden")
-            return json.dumps({'code': 1, 'errmsg': 'User validate error'})
-        else:
-            if not  util.role(name):
-                logging.getLogger().warning("You are not admin,Request forbiden")
-                return json.dumps({'code':1,'errmsg':'You are not admin,Request forbiden'})
-    except:
-        logging.getLogger().warning("Validate error: %s" % traceback.format_exc())
-        return json.dumps({'code': 1, 'errmsg': 'User validate error'})
-    
-    if request.method == 'GET':
-        try:
-            users = []
-            if request.args.get('offset')  is not  None or request.args.get('size') is  not None:
-                offset = request.args.get('offset')
-                size = request.args.get('size')
-                if int(size) >= 1000:
-                    return json.dumps({'code': 1, 'errmsg': 'you input size too big '})
-
-            fields = ['id', 'username', 'name', 'email', 'mobile','role']
-            sql = "SELECT %s FROM user limit %d,%d" % (','.join(fields),int(offset),int(size))
-            app.config['cursor'].execute(sql)
-            for row in app.config['cursor'].fetchall():
-                user = {}
-                for i, k in enumerate(fields):
-                    user[k] = row[i]
-                users.append(user)
-
-            app.config['cursor'].execute("select  count(0) from user")
-            count=int(app.config['cursor'].fetchone()[0])
-            util.write_log(name, 'get_all_users')
-            return json.dumps({'code': 0, 'users': users,'count':count})
-        except:
-            logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
-            return json.dumps({'code': 1, 'errmsg': 'Get users error'})
-
-    elif request.method == 'POST':
-        try:
-            data = request.get_data()
-            data = json.loads(data)
-            fields, values = [], []
-            password = '3b53871ffb407966fc330307500ce968'
-            for k, v in data.items():
-                fields.append(k)
-                values.append("'%s'" % v)
-            sql = "INSERT INTO user (%s,password) VALUES (%s,'%s')" % (','.join(fields), ','.join(values),password)
-            app.config['cursor'].execute(sql)
-            util.write_log(name, "create_user %s" % data['username'])
-            return json.dumps({'code': 0, 'result': 'Create %s success' % data['username']})
-        except:
-            logging.getLogger().error("Create user error: %s" % traceback.format_exc())
-            return json.dumps({'code': 1, 'errmsg': 'Create user error'})
-    return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method})
-
-
-
+   
+    return json.dumps({'code': 1, 'errmsg': "Cannot support '%s' method" % request.method })
--- devops/req.py
+++ devops/req.py
@@ -7,29 +7,31 @@ import requests
 import util
 
 if __name__ == '__main__':
-    s = util.get_validate('sa', '123456')
+    s = util.get_validate('liuziping', '123456')
     headers = {'Authorization': s,'content-type': 'application/json'}
-#    data = {"username": "sa", "email": "sap@123.com", "mobile": "87654321", "name": "sa"}
-    # for get one user, update and delete user
-#    url = "http://192.168.1.243:1000/api/user/3" 
-#    r = requests.get(url, headers=headers)     #for get one user
-#    r = requests.delete(url, headers=headers) #for delete one user
-#    r = requests.put(url, headers=headers,json=json.dumps(data))
+#    data = {"username": "test", "email": "sap@123.com", "mobile": "223210000", "name": "test"}
+
  
-   # for get all user, create user
-#    url = "http://192.168.1.243:1000/api/user" 
-#    r = requests.get(url, headers=headers)      #for get all user
-#    r = requests.post(url, headers=headers,data=json.dumps(data))  #for create user
+   # select user info  or user list 
+#    url = "http://192.168.1.243:1000/api/user?offset=0&size=3" 
+    url = "http://192.168.1.243:1000/api/user" 
+    r = requests.get(url, headers=headers)      #for get all user
+    # update  create  delete user 
+#    r = requests.put(url, headers=headers,json=json.dumps(data))
+#    r = requests.post(url, headers=headers,json=json.dumps(data))  #for create user
+#    data = {"user_id":8}
+#    r = requests.delete(url, headers=headers,json=json.dumps(data)) #for delete one user
 
     # for change password
-#    data = {"oldpassword":"123456", "password":"13456"}
-#    url = "http://192.168.1.243:1000/api/password/7" 
+#    data = {"password":"123456","user_id":7}    #for  admin
+#    data = {"oldpassword":"123456","password":"123456"}  #for user
+#    url = "http://192.168.1.243:1000/api/password" 
 #    r = requests.put(url, headers=headers,json=json.dumps(data))
     
     # for change role
-    data = {"role":1}
-    url = "http://192.168.1.243:1000/api/role/7" 
-    r = requests.put(url, headers=headers,json=json.dumps(data))
+#    data = {"role":1}
+#    url = "http://192.168.1.243:1000/api/role/5" 
+#    r = requests.put(url, headers=headers,json=json.dumps(data))
 
     print r.status_code
     print r.text

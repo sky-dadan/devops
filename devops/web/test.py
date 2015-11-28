fb26b0181a58f9411b839667d39d01d718a3aba8
zp <you@example.com>
2015-11-28 11:27:58 +0800

    更新前端页面

--- devops/web/test.py
+++ devops/web/test.py
@@ -3,17 +3,28 @@ from flask import Flask, render_template
 from  . import app  #导入__init__包文件中实例化的app，等价 from web import app
 from db  import Cursor    #导入数据库连接模块
 
+##   for user 
 @app.route("/",methods=['GET'])
-@app.route("/accounts",methods=['GET'])
+@app.route("/user/info",methods=['GET'])
 def index():
     name = "liuziping"
     return render_template('index.html',name=name)
 
+@app.route("/user/passwd",methods=['GET','POST'])
+def change_passwd():
+    return render_template('change_passwd.html')
 
-@app.route("/accounts/edit",methods=['GET','POST'])
+@app.route("/user/edit",methods=['GET','POST'])
 def user_edit():
-    return render_template('edit.html')
+    return render_template('user_edit.html')
 
-@app.route("/accounts/add",methods=['GET','POST'])
+##  for admin
+@app.route("/user/add",methods=['GET','POST'])
 def user_add():
-    return render_template('add.html')
+    return render_template('user_add.html')
+
+
+@app.route("/user/list",methods=['GET','POST'])
+def user_list():
+    return render_template('user_list.html')
+

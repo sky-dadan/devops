51ba1b265c5db6a0e025c85121f2d87e9c051d5e
zp <you@example.com>
2015-12-02 20:21:56 +0800

    编写用户信息显示及session保存并判断登录状态的功能

--- devops/web/test.py
+++ devops/web/test.py
@@ -1,14 +1,26 @@
 #coding:utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template,session,redirect
 from  . import app  #导入__init__包文件中实例化的app，等价 from web import app
 from db  import Cursor    #导入数据库连接模块
+import util
+import requests,json
 
+headers = {'content-type': 'application/json'}
 ##   for user 
 @app.route("/",methods=['GET'])
 @app.route("/user/info",methods=['GET'])
 def index():
-    name = "liuziping"
-    return render_template('index.html',name=name)
+    if session.get('username') == None:
+       return redirect('/login')
+    headers['authorization'] = session['author']
+    name = session['username']
+    url = "http://192.168.1.243:1000/api/user"
+    r = requests.get(url, headers=headers)      
+    result = json.loads(r.content)
+    if int(result['code']) == 0:
+        return render_template('index.html',name=name,result=result['user'])
+    else:
+        return render_template('index.html')
 
 @app.route("/user/passwd",methods=['GET','POST'])
 def change_passwd():

255afd9a8473cca21a2c3e26d85901c0cc1341c3
zhangxunan <zhangxunan@yuanxin-inc.com>
2015-12-22 18:52:44 +0800

    增加getinfo.py，此文件功能为获取机房等信息

---
+++ devops/web/getinfo.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+from flask import Flask, render_template,session,redirect,request
+from  . import app  
+import requests,json 
+import util,urllib
+
+headers = {"Content-Type": "application/json"}
+url = 'http://127.0.0.1:1000/api'
+data = {
+        "jsonrpc": "2.0",
+        "id":1,
+}
+
+@app.route('/idc',methods=['GET','POST'])
+def idc():
+    headers['authorization'] = session['author']
+    data['method'] = 'idc.getlist'
+    data['params'] = {'output':['id','idc_name']}
+    r = requests.post(url,headers=headers,json=data)
+    return r.text

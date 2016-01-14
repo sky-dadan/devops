66ccf8470eb73b8d0fa9517826ec94736677b4c7
zp <you@example.com>
2016-01-14 14:57:52 +0800

    添加用户登录错误提示信息

--- devops/web/login.py
+++ devops/web/login.py
@@ -14,24 +14,25 @@ def login():
         password = request.form.get('user_pwd')
         url = "http://192.168.1.243:1000/api/auth?username=%s&passwd=%s" % (username,password)
         r = requests.get(url, headers=headers)      #for get all user
-#        return r.content
         result = json.loads(r.content)
         if result['code'] == 0:
             token = result["authorization"]
             res = util.validate(token,app.config['passport_key'])  #返回json对象
-            res = json.loads(res)  #return : dict(username:*,uid:*,role:*)
+            res = json.loads(res)                        #return : dict(username:*,uid:*,role:*)
             if int(res['role']) == 0:                    #针对管理员和普通用户的前端页面展示，暂不处理
                 print "%s is admin" % res['username']
             else:
                 print "%s is user" % res['username']
             session['author'] = token
             session['username'] = res['username']
-            return redirect('/')
+           # return redirect('/')
+	    return json.dumps({'code':0})
         else:
-            return redirect('/login')
+            #return redirect('/login')
+            return json.dumps({'code':1,'errmsg':'用户名或密码错误'})
     return render_template('login.html')
 
 @app.route("/logout",methods=['GET','POST'])
 def logout():
     session.pop('username',None)
-    return redirect('/login')
+    return redirect('/login')  

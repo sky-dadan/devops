0adb56ceb08bb42c6df94ba222378c52e7b92813
zp <you@example.com>
2016-02-22 17:01:06 +0800

    用户update list功能flask部分修改为jsonrpc完毕

--- devops/api/user_perm.py
+++ devops/api/user_perm.py
@@ -7,7 +7,7 @@ import logging, util
 from auth import auth_login
 import json, traceback,hashlib
 
-#本文件只为个人中心提供 用户所在组，所有权限的显示
+#本模块提供用户信息的增删改查，以及 用户所在组，所有权限的查询
 
 #处理将多选参数转为列表 传入的参数[u'1,2'] or  [u'1,2', u'1,3,4']，结果为['1','2','3','4']
 def getid_list(ids):   
@@ -20,6 +20,106 @@ def getid_list(ids):
     id_list.sort()
     print id_list
     return  id_list
+
+#创建用户
+@jsonrpc.method('user.create')
+@auth_login
+def createuser(auth_info,**kwargs):
+    if auth_info['code'] == 1:
+        return  json.dump(auth_info)
+    username = auth_info['username']
+    try:
+        data = request.get_json()['params']
+        print data 
+        data.pop('repwd')    #因为表单是整体打包过来的，第二次输入的密码字段不存在，需要删除
+        data['password'] = hashlib.md5(data['password']).hexdigest()
+        app.config['cursor'].execute_insert_sql('user', data)
+        util.write_log(username, "create_user %s" % data['username'])
+        return json.dumps({'code': 0, 'result': 'Create %s success' % data['username']})
+    except:
+        logging.getLogger().error("Create user error: %s" % traceback.format_exc())
+        return json.dumps({'code': 1, 'errmsg': 'Create user error'})
+
+#获取(uid or username)个人信息，个人信息展示和个人修改信息，以及用户列表中更新用户信息部分的获取数据
+@jsonrpc.method('user.get')
+@auth_login
+def userinfo(auth_info,**kwargs):
+    if auth_info['code'] ==  1:
+        return  json.dump(auth_info)
+    username = auth_info['username']
+    try:
+        output = ['id','username','name','email','mobile','role','is_lock','r_id']
+        fields = kwargs.get('output',output) #api可以指定输出字段，如果没有指定output，就按默认output输出
+        where = kwargs.get('where',None)     #前端传来的where条件，可能是uid或者username
+        result = app.config['cursor'].get_one_result('user', fields, where)
+        if result is '':
+            return json.dumps({'code':1, 'errmsg':'must need give a  where field'})
+        util.write_log(username, 'get_one_user info') 
+        return json.dumps({'code':0,'result':result})
+    except:
+        logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
+        return json.dumps({'code':1,'errmsg':'Get  users error'})
+
+#获取用户列表
+@jsonrpc.method('user.getlist')
+@auth_login
+def userlist(auth_info,**kwargs):
+    if auth_info['code'] ==  1:
+        return  json.dump(auth_info)
+    username = auth_info['username']
+    role = int(auth_info['role'])
+    users = []
+    fields = ['id','username','name','email','mobile','role','is_lock','r_id']
+    if role !=0:
+        return json.dumps({'code': 1,'errmsg':'you not admin ,Cannot look userlist' })
+    try:
+        #获取组所有的id,name并存为字典如：{'1': 'sa', '2': 'php'}
+        gids = app.config['cursor'].get_results('groups', ['id', 'name'])
+        gids = dict([(str(x['id']), x['name']) for x in gids])
+        result = app.config['cursor'].get_results('user', fields)
+        for user in result: #查询user表中的r_id,与groups表生成的字典对比，一致则将id替换为name,如，"sa,php"
+            user['r_id'] = ','.join([gids[x] for x in user['r_id'].split(',') if x in gids])
+            users.append(user)
+        util.write_log(username, 'get_all_users')
+        return  json.dumps({'code': 0, 'users': users,'count':len(users)})
+    except:
+        logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
+        return json.dumps({'code':1,'errmsg':'Get users error'})
+
+
+#更新用户信息
+@jsonrpc.method('user.update')
+@auth_login
+def userupdate(auth_info, **kwargs):
+    if auth_info['code'] == 1:
+        return json.dumps(auth_info)
+    username = auth_info['username']
+    if auth_info['role'] != '0':
+        return json.dumps({'code':1,'errmsg':'you are not admin!'})
+    try:
+        data = request.get_json()['params']
+        where = data.get('where',None)
+        data = data.get('data',None)
+        print data,where
+        if int(auth_info['role']) == 0:
+            result = app.config['cursor'].execute_update_sql('user', data, where)
+        else:
+            result =  app.config['cursor'].execute_update_sql('user', data, where,['name','username','email','mobile'])
+        if result == '':
+            return json.dumps({'code':1, 'errmsg':'you need give an id!'})
+        util.write_log(username, 'update groups %s success!' % data['name'])
+        return json.dumps({'code':0,'result':'update  %s successed' % data['name']})
+    except:
+        logging.getLogger().error("update error: %s"  % traceback.format_exc())
+        return json.dumps({'code':1, 'errmsg':"error : %s" % traceback.format_exc()})
+
+
+#删除用户
+
+#修改密码
+
+#用户登录
+
 
 #用户登录成功后通过用户username获取自己的权限信息，个人中心用户权限部分使用，点击权限名会连接到响应的url
 @jsonrpc.method('user_perm.getlist')
@@ -41,16 +141,16 @@ def getlist(auth_info,**kwargs):
 
         result = app.config['cursor'].get_results('power', fields, {'id': perm_result})
         util.write_log(username, "get permission success")
-        return json.dumps({'result':0,'result':result,'count':len(result)})
+        return json.dumps({'resu lt':0,'result':result,'count':len(result)})
     except:
         logging.getLogger().error("get list permission error: %s"  %  traceback.format_exc())
-        return json.dumps({'code':1,'errmsg':'getlist error : %s'  % traceback.format_exc()})
+        return json.dumps({'cod e':1,'errmsg':'getlist error : %s'  % traceback.format_exc()})
 
 #用户登录成功后通过用户username获取自己的组信息，个人中心调用
 @jsonrpc.method('user_groups.getlist')
 @auth_login
 def user_groups(auth_info, **kwargs):
-    if auth_info['code'] == 1:
+    if auth_info['code'] == 1: 
         return json,dumps(auth_info)
     username = auth_info['username']
     try:
@@ -88,57 +188,6 @@ def get_color(auth_info, **kwargs):
         logging.getLogger().error('groups_sel.get error!')
         return json.dumps({'code':1,'errmsg':'error: %s'  %  traceback.format_exc()})
 
-#创建用户
-@jsonrpc.method('user.create')
-@auth_login
-def createuser(auth_info,**kwargs):
-    if auth_info['code'] == 1:
-        return  json.dump(auth_info)
-    username = auth_info['username']
-    try:
-        data = request.get_json()['params']
-        print data 
-        data.pop('repwd')    #因为表单是整体打包过来的，第二次输入的密码字段不存在，需要删除
-        data['password'] = hashlib.md5(data['password']).hexdigest()
-        app.config['cursor'].execute_insert_sql('user', data)
-        util.write_log(username, "create_user %s" % data['username'])
-        return json.dumps({'code': 0, 'result': 'Create %s success' % data['username']})
-    except:
-        logging.getLogger().error("Create user error: %s" % traceback.format_exc())
-        return json.dumps({'code': 1, 'errmsg': 'Create user error'})
-#获取个人信息，用户中心个人信息展示和个人修改信息，以及用户列表中更新用户信息部分的获取数据
-@jsonrpc.method('user.get')
-@auth_login
-def userinfo(auth_info,**kwargs):
-    if auth_info['code'] == 1:
-        return  json.dump(auth_info)
-    username = auth_info['username']
-    try:
-        output = ['id','username','name','email','mobile','role','is_lock','r_id']
-        fields = kwargs.get('output',output) #api可以指定输出字段，如果没有指定output，就按默认output输出
-        where = kwargs.get('where',None)     #前端传来的where条件，可能是uid或者username
-        result = app.config['cursor'].get_one_result('user', fields, where)
-        if result is None:
-            return json.dumps({'code':1, 'errmsg':'must need give a  where field'})
-        util.write_log(username, 'get_one_user info') 
-        return json.dumps({'code':0,'result':result})
-    except:
-        logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
-        return json.dumps({'code':1,'errmsg':'Get  users error'})
-
-        
-
-#获取用户列表
-
-#通过id获取单条记录信息
-
-#更新用户信息
-
-#删除用户
-
-#修改密码
-
-#用户登录
 '''
 #通过用户的id获取到某个用户的所有权限内容，不会根据个人对权限做更新，没什么用。且与上面代码冗余，需要整合
 @jsonrpc.method('user_perm.get')
--- devops/web/user.py
+++ devops/web/user.py
@@ -6,7 +6,7 @@ import util
 import requests,json
 
 headers = {'content-type': 'application/json'}
-#获取个人信息 
+#个人中心web页面 
 @app.route("/",methods=['GET'])
 @app.route("/user/info",methods=['GET'])
 def index():
@@ -14,88 +14,31 @@ def index():
         return redirect('/login')
     headers['authorization'] = session['author']
     name = session['username']
-    #url = "http://%s/api/user" % app.config['api_host']
-    #r = requests.get(url, headers=headers)      
-    #result = json.loads(r.content)
-    #if int(result['code']) == 0:
-    #    return render_template('index.html',name=name,result=result['user'])
     return render_template('index.html',name=name)
 
-
-#获取个人信息，为后面的编辑获取数据,需要和管理员获取信息合并
-@app.route("/user/edit",methods=['GET','POST'])
-def user_edit():
-    if session.get('username') == None:
-        return redirect('/login')
-    headers['authorization'] = session['author']
-    name = session['username']
-    url = "http://%s/api/user" % app.config['api_host']
-    r = requests.get(url, headers=headers)      
-    result = json.loads(r.content)
-    if int(result['code']) == 0:
-        return render_template('user_edit.html',name=name,user=result['user'])
-    else:
-        return render_template('user_edit.html',name=name,errmsg=result['errmsg'])
-
-##管理员添加用户
+##管理员添加用户web页面
 @app.route("/user/add",methods=['GET','POST'])
 def useradd():
     if session.get('username') == None:
         return redirect('/login')
     headers['authorization'] = session['author']
     name = session['username']
-    '''
-    if request.method == 'POST':
-        r_id = request.form.getlist('r_id')     #获取多一个属性r_id的多个值，保存为列表
-        r_id = ','.join(r_id)
-        if not r_id:
-            return json.dumps({'code':1,'errmsg':'你必须选择一个所属组!'})
-        kv = [('username', 'username'), ('name', 'user_name'), ('email', 'user_mail'), ('mobile', 'user_phone'), ('role', 'role'), ('is_lock', 'lock'), ('password', 'user_pwd')]
-        data = dict([(k, request.form.get(v)) for k,v in kv])
-        if len(data['password']) < 6:
-            return json.dumps({'code':1, 'errmsg': '密码至少需要6位!'})
-        elif data['password'] != request.form.get('user_repwd'):
-            return json.dumps({'code':1, 'errmsg': '两次输入的密码不一致!'})
-        data['r_id'] = r_id
-        url = "http://%s/api/user" % app.config['api_host']
-
-        r = requests.post(url,headers=headers,json=data)
-        return r.content
-    else:
-    '''    
     return render_template('user_add.html',name=name)
  
-#管理员查看用户列表
+#管理员查看用户列表web页面，
 @app.route("/user/list",methods=['GET','POST'])
 def user_list():
     if session.get('username') == None:
         return redirect('/login')
     headers['authorization'] = session['author']
     name = session['username']
-    url = "http://%s/api/user?list=true" % app.config['api_host']
-    r = requests.get(url, headers=headers)      
-    result = json.loads(r.content)
-    if int(result['code']) == 0:
-        return render_template('user_list.html',name=name,users=result['users'])
-    else:
-        return render_template('user_list.html',errmsg=result['errmsg'])
+    return render_template('user_list.html',name=name)
 
-#获取更新单条记录的信息
-@app.route("/getbyid",methods=['GET','POST'])
-def getbyid():
-    if session.get('username') == None:
-        return redirect('/login')
-    id = int(request.args.get('id'))     
-    headers['authorization'] = session['author']
-    url = "http://%s/api/user/getbyid/%d" % (app.config['api_host'], id)
-    r = requests.get(url, headers=headers)      
-    result = json.loads(r.content)
-    return json.dumps(result)
 
 #管理员更新用户信息
 @app.route("/user/update",methods=['GET','POST'])
 def user_update():
-    if session.get('username') == None:
+    if session.get('usern ame') == None:
         return redirect('/login')
     headers['authorization'] = session['author']
 
@@ -111,60 +54,6 @@ def user_update():
     r = requests.put(url, headers=headers,json=json.dumps(data))
     result = json.loads(r.content)
     return  json.dumps(result)
-
-#用户更新个人信息
-@app.route("/user/updateoneself",methods=['GET','POST'])
-def updateoneself():
-    if session.get('username') == None :
-        return redirect('/login')
-    headers['authorization'] = session['author']
-    data = dict([(x, request.args.get('user_%s' % x)) for x in ('name', 'email', 'mobile')])
-    url = "http://%s/api/user" % app.config['api_host']
-    r = requests.put(url,headers=headers,json=json.dumps(data))
-    return r.content
-
-#管理员修改用户密码
-@app.route("/user/changepasswd",methods=['GET','POST'])
-def changepasswd():
-    if session.get('username') == None :
-        return redirect('/login')
-    headers['authorization'] = session['author']
-    if request.method == 'POST':
-        user_id = int(request.form.get('passwdid'))
-        password = request.form.get('changepasswd')
-        data = {'user_id':user_id,'password':password}
-        url = "http://%s/api/password" % app.config['api_host']
-        r = requests.put(url, headers=headers,json=json.dumps(data))
-        result = json.loads(r.content)
-        return json.dumps(result)
-
-#用户修改个人密码
-@app.route("/user/chpwdoneself",methods=['GET','POST'])
-def chpwdoneself():
-    if session.get('username') == None:
-        return redirect('/login')
-    headers['authorization'] = session['author']
-    if request.method == 'POST':
-        oldpasswd = request.form.get('oldpasswd')
-        newpasswd = request.form.get('newpasswd')
-        data = {'oldpassword':oldpasswd,'password':newpasswd}
-        url = "http://%s/api/password" % app.config['api_host']
-        r = requests.put(url, headers=headers,json=json.dumps(data))
-        result = json.loads(r.content)
-        return json.dumps(result)
-
-#管理员删除用户
-@app.route("/user/delete",methods=['GET','POST'])
-def userdelete():
-    if session.get('username') == None :
-        return redirect('/login')
-    headers['authorization'] = session['author']
-    user_id = int(request.args.get('id'))
-    url = "http://%s/api/user" % app.config['api_host']
-    data = {"user_id":user_id}
-    r = requests.delete(url, headers=headers,json=json.dumps(data))
-    result = json.loads(r.content)
-    return json.dumps(result)
 
 #角色列表web页面，数据走cmdb.py中统一的JSONRPC, 后期需要整合
 @app.route("/role/list",methods=['GET','POST'])

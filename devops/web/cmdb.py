1bd35f93653d89ccf5f018e2683fc4f46b8dc784
zp <you@example.com>
2016-02-23 15:14:56 +0800

    修复cmdb.py getapi个人中心获取数据和服务管理获取不兼容的问题

--- devops/web/cmdb.py
+++ devops/web/cmdb.py
@@ -76,7 +76,7 @@ def addapi():
 def getapi():
     headers['authorization'] = session['author']
     method = request.args.get('method')
-    username = request.args.get('username')        #获取的是url传过来的username,不是当前session的username
+    username = request.args.get('username',session['username']) #获取的是url传过来的username,如果没有取session
     uid = request.args.get('id')
     if uid is not None:
         data['params'] = {"where":{'id':uid}}

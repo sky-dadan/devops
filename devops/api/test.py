c3753b1872aaf088cff635de17cf4742123bf08c
zp <you@example.com>
2016-03-02 11:47:42 +0800

    添加邮件通知

--- devops/api/test.py
+++ devops/api/test.py
@@ -2,7 +2,7 @@
 #coding: utf-8
 from flask import Flask, request
 from . import app
-import logging, util
+import logging, util,mail
 import json, traceback,os
 from auth import auth_login
 
@@ -100,13 +100,21 @@ def gitolite():
             #将项目信息写入配置文件
             str2 = ""
             for k,v in project.items():
-                    str2 += "repo %s \n    RW+ = @%s %s \n    RW = @%s %s \n" % (k,v['group_all_perm'],v['user_all_perm'],v['group_rw_perm'],v['user_rw_perm'])
+                    str2  += "repo %s \n    RW+ = @%s %s \n    RW = @%s %s \n" % (k,v['group_all_perm'],v['user_all_perm'],v['group_rw_perm'],v['user_rw_perm'])
             with open("/root/gitconfig/gitolite-admin/conf/gitolite.conf",'a') as f:
                 f.write(str2)
 
             #git add/commit/push生效
             #os.system("cd ** && git add && git commit -m 'update git conf' && git push origin master")
-            return json.dumps({'code':0,'errmsg':"git操作成功"})
+            
+            #给相关人员发邮件,在主程序中调用,格式如下
+            subject = "devops项目创建成功"
+            content = "项目地址为：%s" % ("devops")
+            smtp_to = ['sa@yuanxin-inc.com','liuziping@yuanxin-inc.com']
+            mail.sendmail(subject,content,smtp_to)
+
+
+            return json.dumps({'code':0,'result':"git操作成功"})
         except:
             logging.getLogger().error("get config error: %s" % traceback.format_exc())
             return json.dumps({'code':1,'errmsg':"写配置文件报错"})
--- devops/mail.py
+++ devops/mail.py
@@ -3,7 +3,6 @@
 '''
     Use:   
         import mail 
-
 	subject = "这是一封测试邮件"
 	content = "这是一封测试邮件"
 	smtp_to=['liuziping@yuanxin-inc.com','787696331@qq.com']
@@ -17,8 +16,8 @@ import os,traceback
 
 smtp_host="smtp.mxhichina.com"
 smtp_port=25                                      #默认就是端口就是25,可不写
-smtp_login=os.environ.get('smtp_login')           #登录用户名
-smtp_passwd=os.environ.get('smtp_passwd')         #登录密码，账号密码信息千万不要写入脚本，可以通过export将信息导入环境变量，脚本从环境变量中获取
+smtp_login=os.environ.get('login','sa-notice@yuanxin-inc.com')           #登录用户名
+smtp_passwd=os.environ.get('passwd','Miao13456')         #登录密码，账号密码信息千万不要写入脚本，可以通过export将信息导入环境变量，脚本从环境变量中获取
 smtp_from='sa-notice@yuanxin-inc.com'           #发件人
 
 def sendmail(subject,content,smtp_to):
@@ -36,10 +35,10 @@ def sendmail(subject,content,smtp_to):
 	except Exception,e:
 		 print "Send mail error: %s" % traceback.format_exc()
 #  for  test
-#if __name__=='__main__':
-#	subject = "这是一封测试邮件"
-#	content = "这是一封测试邮件"
-#	smtp_to=['liuziping@yuanxin-inc.com','787696331@qq.com']
-#	sendmail(subject,content,smtp_to)
+if __name__=='__main__':
+	subject = "这是一封测试邮件"
+	content = "这是一封测试邮件"
+	smtp_to=['liuziping@yuanxin-inc.com','787696331@qq.com']
+	sendmail(subject,content,smtp_to)
 
 

0acf9b23aca0b232d7cf964ac89e01c1e4a127b0
zhangxunan <zhangxunan@yuanxin-inc.com>
2016-03-25 15:36:51 +0800

    project_test.py

--- devops/api/project_test.py
+++ devops/api/project_test.py
@@ -14,6 +14,7 @@ from user_perm import getid_list
 #headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
 headers = {"Content-Type": "application/json"}
 
+#插入一条上线记录
 @jsonrpc.method('project_test.create')
 @auth_login
 def project_test_create(auth_info, **kwargs):
@@ -49,6 +50,11 @@ def project_test_getlist(auth_info, **kwargs):
         data = request.get_json()['params']
         where = kwargs.get('where',None)
         result = app.config['cursor'].get_results('project_test',fields,where,'push_date',False)
+        print "result =",result
+        projects = util.userproject(username)
+        print "projects =",projects
+        for res in result:
+            res['project_id'] = projects[res['project_id']]
         util.write_log(username, '查询项目成功') 
         return json.dumps({'code':0,'result':result},cls=MyEncoder)
     except:
@@ -67,14 +73,13 @@ def project_test_get(auth_info, **kwargs):
         
         fields = ['project_id','commit','pusher','push_date','comment']
         result = []
-        sql = 'select project_id,commit, pusher, push_date,comment from (select project_id, commit, pusher, push_date,comment from project_test order by push_date desc) as sort_project_test group by project_id'
+        sql = 'SELECT %s FROM (SELECT %s FROM project_test ORDER BY push_date DESC) AS sort_project_test GROUP BY project_id' %(','.join(fields),','.join(fields))
         app.config['cursor'].execute(sql)
         for row in  app.config['cursor'].fetchall():
             res = {}
             for i,k in enumerate(fields):
                 res[k] = row[i]
             result.append(res)
-        print "result =",result
         util.write_log(username, '查询项目成功') 
         return json.dumps({'code':0,'result':result},cls=MyEncoder)
     except:

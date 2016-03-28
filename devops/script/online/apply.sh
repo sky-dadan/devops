a82b906ab556055a891a47a89026027009eaf820
liuziping <liuziping@yuanxin-inc.com>
2016-03-28 16:01:49 +0800

    添加获取git信息的脚本

--- devops/script/online/apply.sh
+++ devops/script/online/apply.sh
@@ -14,5 +14,15 @@ cd $1
 git pull>/dev/null
 commit=`git log |awk  'NR==1{ print substr($2,1,6)}'`
 tag=`git log |awk  'NR==11{for(i=1;i<=NF;i++) printf $i " "}'`
-echo "$commit $tag"
+echo "$commit"
+echo "$tag"
+
+##########  python 脚本调用 ################################################
+# cat test.py 
+# import subprocess
+#
+# p = subprocess.Popen(['sh','apply.sh','liuziping'],stdout=subprocess.PIPE)
+# commit = p.stdout.readline().rstrip('\n')    #获取commit
+# tag = p.stdout.readline().rstrip('\n')       #获取tag
+ 
 

f1899e86493a9ffeec86eda3e1fd03a0da218895
zhangxunan <zhangxunan@yuanxin-inc.com>
2016-03-31 16:07:24 +0800

    edit online_test_dev.sh

--- devops/script/online/online_test_dev.sh
+++ devops/script/online/online_test_dev.sh
@@ -13,7 +13,8 @@ if [ $# != 2 ];then    #执行输入不正确会提示
    echo "执行方法 ./online.sh  project_name host"
    exit
 fi
-
+#将ip地址以逗号分隔(192.168.1.253,182.18.40.230)改为空格分隔（192.168.1.253 182.168.40.230）
+ips=$(echo $a  | awk -F',' '{for(i=1;i<=NF;i++){printf("%s ",$i) } printf("\n")}')
 cd /data/gitclone_dev   
 if  [ ! -d $1 ];then    #判断gitclone中项目是否存在，不存在就gitclone一份，如果存在了，就直接git pull
     git clone -b dev http://192.168.1.251:8000/git/$1 &>/dev/null
@@ -23,7 +24,11 @@ if  [ ! -d $1 ];then    #判断gitclone中项目是否存在，不存在就gitcl
     info=$(git log --oneline -1 --pretty=format:"%s")
     echo $commit,$info
 #    chown www:www -R /data/gitclone_dev/$1
-#    /usr/bin/rsync -avz --timeout=20  -e ssh  --exclude=.git --exclude=.svn   /data/gitclone_dev/$1 root@$2:/data/wwwroot/    #将新项目整个目录rsync到远程主机目录
+#    for ip in $ips
+#    do
+#将新项目整个目录rsync到远程主机目录
+#    /usr/bin/rsync -avz --timeout=20  -e ssh  --exclude=.git --exclude=.svn   /data/gitclone_dev/$1 root@$ip:/data/wwwroot/ 
+#    done
 else
     cd /data/gitclone_dev/$1  
     git pull &>/dev/null
@@ -31,6 +36,9 @@ else
     commit=$(git log  --oneline -1 --pretty=format:"%h")
     info=$(git log --oneline -1 --pretty=format:"%s")
     echo $commit,$info
-#    /usr/bin/rsync -avz  -e ssh  --exclude=.git --exclude=.svn --exclude=runtime  --include=assets/js --include=assets/css --include=assets/images --include=assets/wechat  --exclude=assets/*/  /data/gitclone_dev/$1/ root@$2:/data/wwwroot/$1/    #将项目目录的内容rsync到远程主机目录下
-
+#    for ip in $ips
+#    do
+#将项目目录的内容rsync到远程主机目录下
+#    /usr/bin/rsync -avz  -e ssh  --exclude=.git --exclude=.svn --exclude=runtime  --include=assets/js --include=assets/css --include=assets/images --include=assets/wechat  --exclude=assets/*/  /data/gitclone_dev/$1/ root@$ip:/data/wwwroot/$1/   
+#    done
 fi    

6a63facf3474c7dc2ac25ee4f4556ee588d6b799
sky-du <duminchao@yuanxin-inc.com>
2016-08-03 17:01:08 +0800

    修改优化工单功能

--- devops/api/opsjobs.py
+++ devops/api/opsjobs.py
@@ -16,15 +16,15 @@ def opsjob_create(auth_info, **kwargs):
     username = auth_info['username']
     try:
         data = request.get_json()['params']
+        util.write_log(data, ' request.get_json() params')
         data['apply_date'] = time.strftime('%Y-%m-%d %H:%M')
         data['status'] , data['apply_persion'] = 0, username
         app.config['cursor'].execute_insert_sql('ops_jobs',data)
         '''sendemail'''
         smtp_to = ['sa@yuanxin-inc.com']              #给运维部门发送邮件
-        send_info = '%s提交工单申请，运维同事请及时处理!'  % username
+        send_info = '%s提交工单申请，运维同事请及时处理!'  % username  + '\n' + '工单申请描述\n' + data['apply_desc']
         util.sendmail(app.config, smtp_to,'圆心运维工单申请',send_info)
         util.write_log(username, "create opsjonb  create")
-        print data
         return json.dumps({'code':0,'result':'创建工单成功!'})
     except:
         logging.getLogger().error('opsjob create error: %s' % traceback.format_exc())
@@ -56,11 +56,10 @@ def opsjob_getadmin(auth_info,**kwargs):
         return json.dumps(auth_info)
     try:
         username = auth_info['username']
-        where = {'status':['2','3']}
+        where = {'status':['0','1']}
         fields = ['id','apply_date','apply_type','apply_desc','deal_persion','status','deal_desc','deal_time','apply_persion']
         result = app.config['cursor'].get_results('ops_jobs',fields,where)
-        print result
-        util.write_log(username, 'get opsjob status in (2,3) success!') 
+        util.write_log(username, 'get opsjob status in (0 ,1) success!') 
         return json.dumps({'code':0,'result':result,'count':len(result)},cls=MyEncoder)
     except:
         logging.getLogger().error("select opsjob status in (2,3) error : %s"  % traceback.format_exc())
@@ -86,7 +85,7 @@ def opsjob_list(auth_info,**kwargs):
 
 
 #工单处理
-@jsonrpc.method('opsjob.deal')
+@jsonrpc.method('opsjob.update')
 @auth_login
 def opsjob_deal(auth_info, **kwargs):
     if auth_info['code'] == 1:
@@ -96,24 +95,26 @@ def opsjob_deal(auth_info, **kwargs):
     try:
         username = auth_info['username']
         data = request.get_json()['params']   # data = {'where':{'id':3},'status':0,'deal_desc':'deal desc comment'}
-        status = data.get('status',None)
+        status = kwargs.get('status')
         data['deal_persion'] = username
-        where = data['where']
+        where = kwargs.get('where')
+        print status, where
+        util.write_log(data, 'data de shu ju lei xing')
         result = app.config['cursor'].get_one_result('ops_jobs',['apply_persion','apply_type','apply_desc'],where)
         data.pop('where')
-        if status is None:       #工单改为处理状态
+        if status is not None and status == '0':       #工单改为处理状态
             data['status'] = 1
             app.config['cursor'].execute_update_sql('ops_jobs',data,where)
-            util.write_log(username, ' opsjobs 正在处理中......')
+            util.write_log(result['apply_persion'], '提交的工单正在处理中......')
             smtp_to = [result['apply_persion']+'@yuanxin-inc.com']
-            smtp_info = '%s您提交的工单正在处理中.......'  % result['apply_persion']
+            smtp_info = '%s您提交的工单正在处理中.......\n %s'  % (result['apply_persion'],result['apply_desc'])
             util.sendmail(app.config,smtp_to, '圆心运维工单',smtp_info)
-            print  data,where
             return json.dumps({'code':0,'result':'工单正在处理中......'})
         else:                          #工单处理结束
             data['deal_time'] = time.strftime("%Y-%m-%d %H:%M")
             app.config['cursor'].execute_update_sql('ops_jobs',data,where)
             if result['apply_type'] == 1:        #DB类型的修改操作，需要通知测试组
+                util.write_log('DB类型修改，同时给测试组发送邮件.....................')
                 smtp_to = ['test@yuanxin-inc.com',result['apply_persion']+'@yuanxin-inc.com']
             else:
                 smtp_to = [result['apply_persion']+'@yuanxin-inc.com']
